cf207b02932ad54cf6b7a6525f1399861c1ae43d
Ian Pang <ian.eng10@gmail.com>
2023-04-04 00:40:49 -0600

    Version estable 3.0

--- Main.java
+++ Main.java
@@ -1,21 +1,38 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class proceso {
     // Atributos
     public String id, nombre;
     public Integer size, teje, prioridad, tllega;
-
+    
     // Constructor
-    public proceso(String id, String nombre, Integer size,
-                        Integer teje, Integer prioridad, Integer tllega){
+    public proceso(){
+    }
+    // Dar valor al id
+    public void setId(String id) {
         this.id = id;
+    }
+    // Dar valor al nombre
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    // Dar valor a la prioridad
+    public void setPrioridad(Integer prioridad) {
         this.prioridad = prioridad;
+    }
+    // Dar valor al tamaño
+    public void setSize(Integer size) {
         this.size = size;
+    }
+    // Dar valor al tiempo de ejecucion
+    public void setTeje(Integer teje) {
         this.teje = teje;
+    }  
+    // Dar valor al tiempo de llegada
+    public void setTllega(Integer tllega) {
         this.tllega = tllega;
     }
-
     // Metodo para imprimir con formato los procesos
     public String toString(){
         return "ID: "+this.id+" Nombre: "+this.nombre+" Teje: "+this.teje;
@@ -130,6 +147,7 @@ class colaProcesos {
             // Si coincide su tiempo de llegada con el tiempo total
             if (planeado.get(i).tllega == tiempoTotal) {
                 insertaL(planeado.get(i));
+                mostrarColas();
                 expande();
                 planeado.remove(i);
             }
@@ -149,64 +167,67 @@ class colaProcesos {
             }
             insertaE(iniL.getData());
             memoriaUsada += iniL.getData().size;
+            System.out.println("Subio el proceso "+iniL.getData().id+" y restan "+
+            (memoriaTotal-memoriaUsada)+" unidades de memoria");
             iniL = iniL.getSig();
+            mostrarColas();
         }
-    }
-    // Metodo que limpia la pantalla
-    private void cls(){
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();
     }
     // Metodo que se encarga de mandar a ejecutar cada proceso
     private void procesa(){
-        // Si aun no llega ningun proceso se cuenta el tiempo
-        while (iniE == null) {
-            this.tiempoTotal++;
-            expandePost();
-        }
-        // En cuanto se encuantra un proceso empieza a ejecutarse
-        while (iniE != null) {
-            boolean restado = false;
-            mostrarColas();
-            // Añade los procesos a una lista para poder realizar los calculos de tiempos
-            diagrama.add(new gantt(iniE.getData(), tiempoTotal,
-            iniE.getData().teje, iniE.getData().tllega));
-            // Este ciclo realiza contabiliza el quantum
-            for (int i = 0; i < quantum; i++) {
-                // Si el tiempo de ejecucion no a llegado a 0 se reduce su tiempo en
-                // una unidad y se aumenta el tiempo total, se revisa si ya es tiempo
-                // de llegada de un proceso
+        // Ciclo que mantiene la espera en caso de que no 
+        // halla procesos listos para ejecucion pero si planeados
+        while (planeado.isEmpty() != true) {
+            // Si aun no llega ningun proceso se cuenta el tiempo
+            while (iniE == null) {
+                this.tiempoTotal++;
+                expandePost();
+            }
+            // En cuanto se encuantre un proceso empieza a ejecutarse
+            while (iniE != null) {
+                boolean restado = false;
+                mostrarColas();
+                // Añade los procesos a una lista para poder realizar los calculos de tiempos
+                diagrama.add(new gantt(iniE.getData(), tiempoTotal,
+                iniE.getData().teje, iniE.getData().tllega));
+                // Este ciclo realiza contabiliza el quantum
+                for (int i = 0; i < quantum; i++) {
+                    System.out.println(iniE.getData().id+" en ejecución "+iniE.getData().teje+"msg");
+                    // Si el tiempo de ejecucion no a llegado a 0 se reduce su tiempo en
+                    // una unidad y se aumenta el tiempo total, se revisa si ya es tiempo
+                    // de llegada de un proceso
+                    if (iniE.getData().teje != 0) {
+                        iniE.redux();
+                        this.tiempoTotal++;
+                        expandePost();
+                    // Si el tiempo de ejecucion es 0 se quita el peso del proceso
+                    // se avisa con la bandera que ya no es necesario restarlo y
+                    // se revisa si ya es tiempo de llegada de un proceso
+                    }else{
+                        memoriaUsada -= iniE.getData().size;
+                        restado = true;
+                        expandePost();
+                        expande();
+                        break;
+                    }
+                }
+                // En caso de todavia tener tiempo de ejecucion se vuelve a colocar
+                // en la cola de procesos listos para ejecucion
                 if (iniE.getData().teje != 0) {
-                    iniE.redux();
-                    this.tiempoTotal++;
-                    expandePost();
-                // Si el tiempo de ejecucion es 0 se quita el peso del proceso
-                // se avisa con la bandera que ya no es necesario restarlo y
-                // se revisa si ya es tiempo de llegada de un proceso
+                    insertaE(iniE.getData());
+                // En caso de ya no tener tiempo de ejecucion
                 }else{
-                    memoriaUsada -= iniE.getData().size;
-                    restado = true;
+                    // Se revisa si ya se libero la memoria del proceso
+                    if (restado == false) {
+                        memoriaUsada -= iniE.getData().size;
+                    }
+                    // Se revisa si ya es tiempo de llegada de un proceso
                     expandePost();
                     expande();
-                    break;
                 }
+                // Se avanza al siguiente proceso
+                iniE = iniE.getSig();
             }
-            // En caso de todavia tener tiempo de ejecucion se vuelve a colocar
-            // en la cola de procesos listos para ejecucion
-            if (iniE.getData().teje != 0) {
-                insertaE(iniE.getData());
-            // En caso de ya no tener tiempo de ejecucion
-            }else{
-                // Se revisa si ya se libero la memoria del proceso
-                if (restado == false) {
-                    memoriaUsada -= iniE.getData().size;
-                }
-                // Se revisa si ya es tiempo de llegada de un proceso
-                expandePost();
-                expande();
-            }
-            // Se avanza al siguiente proceso
-            iniE = iniE.getSig();
         }
         // Calculamos los tiempos de espera, respuesta y ejecucion
         calcula();
@@ -267,7 +288,6 @@ class colaProcesos {
     // Metodo que muestra el estado de cada cola de datos, el tiempo
     // de total y el uso de memoria
     private void mostrarColas(){
-        cls();
         ArrayList<String> colaVolteada = new ArrayList<String>();
         nodo temp = iniL;
         // Se extraen los procesos de la cola de procesos listos
@@ -297,10 +317,7 @@ class colaProcesos {
         System.out.println("Proceso en ejecucion");
         System.out.println(colaVolteada.get(0));
         System.out.println("Tiempo total: "+tiempoTotal);
-        System.out.println("Memoria usada: "+memoriaUsada);
-        System.out.println("Presiona cualquier tecla para continuar...");
-        new java.util.Scanner(System.in).nextLine();
-        cls();
+        System.out.println("Memoria restante: "+(memoriaTotal-memoriaUsada)+"\n\n");
     }
 }
 
@@ -308,6 +325,27 @@ class colaProcesos {
 public class Main {
     public static void main(String []args) {
         colaProcesos uno = new colaProcesos(1000, 4);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingresa el numero de procesos a trabajar: ");
+        int numeroP = input.nextInt();
+        for (int i = 0; i < numeroP; i++) {
+            proceso nuevoP = new proceso();
+            System.out.print("Ingresa el id del proceso: ");
+            nuevoP.setId(input.next());
+            System.out.print("Ingresa el nombre del proceso: ");
+            nuevoP.setNombre(input.next());
+            System.out.print("Ingresa tamaño del proceso: ");
+            nuevoP.setSize(input.nextInt());
+            System.out.print("Ingresa el tiempo de ejecucion del proceso: ");
+            nuevoP.setTeje(input.nextInt());
+            System.out.print("Ingresa la prioridad del proceso: ");
+            nuevoP.setPrioridad(input.nextInt());
+            System.out.print("Ingresa el tiempo de llegada del proceso: ");
+            nuevoP.setTllega(input.nextInt());
+            uno.inserta(nuevoP);
+        }
+        System.out.println("\n");
+        input.close();
         // Ejercicio 21 RR
         // uno.inserta(new proceso("1", "powershell",0,25,0,0));
         // uno.inserta(new proceso("2", "cmd",0,9,0,4));
